ed1c65907d145ae5fe4e287bac3824a0e5eba5b8
idk <hankhill19580@gmail.com>
2023-01-16 03:23:06 +0000

    remove some superfluous extensions

--- lv.go
+++ lv.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	goi2pbrowser "github.com/eyedeekay/go-i2pbrowser"
 	"i2pgit.org/idk/railroad/configuration"
@@ -27,6 +28,15 @@ func LaunchView() error {
 	for _, v := range dira {
 		if !v.IsDir() {
 			path := filepath.Join("railroad-admin/i2p.firefox.usability.profile/extensions/", v.Name())
+			if strings.Contains(path, "localcdn") {
+				os.RemoveAll(path)
+			}
+			if strings.Contains(path, "https") {
+				os.RemoveAll(path)
+			}
+			if strings.Contains(path, "{b86e4813-687a-43e6-ab65-0bde4ab75758}") {
+				os.RemoveAll(path)
+			}
 			log.Println(path)
 			os.Chmod(path, 0664)
 		}
